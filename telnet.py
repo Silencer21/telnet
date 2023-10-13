bceec1b2af1e2afe31a30debc993f517b6f32c0d
Silencer21 <145554273+Silencer21@users.noreply.github.com>
2023-10-13 16:24:20 +0100

    Add files via upload

---
+++ telnet.py
@@ -0,0 +1,62 @@
+#import modules
+import pexpect
+
+#define variables
+ip_address = '192.168.56.101'
+username = 'cisco'
+password = 'cisco123!'
+
+#create telnet session
+session = pexpect.spawn('telnet ' + ip_address, encoding='utf-8',timeout=20)
+result = session.expect(['Username:',pexpect.TIMEOUT])
+
+#check if error exists,if yes display and exit
+if result != 0:
+    print('-'*3, 'FAILURE! creating session for: ',ip_address)
+    exit()
+
+#session is expecting username, enter details
+session.sendline(username)
+result = session.expect(['Password: ',pexpect.TIMEOUT])
+
+##check if error exists,if yes display and exit
+if result != 0:
+    print('-'*3, 'FAILURE! entering username: ',username)
+    exit()
+
+#session is expecting password,enter details
+session.sendline(password)
+result = session.expect(['#',pexpect.TIMEOUT])
+
+#check if error exists,if yes display and exit
+if result != 0:
+    print('-'*3, 'FAILURE! entering password: ',password)
+    exit()
+
+#display success message
+
+print('-'*25)
+print('')
+print('-'*3, 'Success connecting to: ',ip_address)
+print('-'*3,              'Username: ',username)
+print('-'*3,              'Password: ', password)
+print('')
+print('-'*25)
+
+#modify hostname
+session.sendline('configure terminal')
+result = session.expect(([r'.\(config\)#',pexpect.TIMEOUT,pexpect.EOF]))
+
+if result != 0:
+    print('-'*3,'FAILURE entering config mode')
+    exit()
+hostname = 'R1'
+session.sendline('hostname ' + hostname)
+result = session.expect(([r'.\(config\)#',pexpect.TIMEOUT,pexpect.EOF]))
+if result != 0:
+    print('-'*3,'FAILURE setting hostname')
+    exit()
+print('Hostname changed to: ',hostname)
+#terminate telnet session
+session.sendline('quit')
+session.close()
