9a21e231481941d3ca37a10999caf1fe46e27423
Silencer21 <145554273+Silencer21@users.noreply.github.com>
2023-10-25 13:04:27 +0100

    Add files via upload

--- telnet.py
+++ telnet.py
@@ -45,18 +45,32 @@ print('-'*25)
 
 #modify hostname
 session.sendline('configure terminal')
-result = session.expect(([r'.\(config\)#',pexpect.TIMEOUT,pexpect.EOF]))
+result = session.expect(([r'\(config\)#',pexpect.TIMEOUT,pexpect.EOF]))
 
 if result != 0:
     print('-'*3,'FAILURE entering config mode')
     exit()
 hostname = 'R1'
 session.sendline('hostname ' + hostname)
-result = session.expect(([r'.\(config\)#',pexpect.TIMEOUT,pexpect.EOF]))
+result = session.expect(([r'\(config\)#',pexpect.TIMEOUT,pexpect.EOF]))
 if result != 0:
     print('-'*3,'FAILURE setting hostname')
     exit()
+result = session.expect(([r'\(config\)#',pexpect.TIMEOUT,pexpect.EOF]))
 print('Hostname changed to: ',hostname)
+
+#show running config
+session.sendline('show running-config')
+result = session.expect(['#',pexpect.TIMEOUT,pexpect.EOF])
+
+if result != 0:
+    print('-'*3,'FAILURE showing running configuration')
+    exit()
+
+#saving to local file 
+with open('config.txt','w') as file:
+    file.write(session.before)
+print('running configuration saved to config.txt')
 #terminate telnet session
 session.sendline('quit')
 session.close()
