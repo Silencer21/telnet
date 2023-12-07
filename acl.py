986e73b22a089e0f77c107e6867749bc6dc3409d
Silencer21 <145554273+Silencer21@users.noreply.github.com>
2023-12-07 22:35:51 +0000

    Update acl.py

--- acl.py
+++ acl.py
@@ -1,3 +1,17 @@
+import pexpect
+device_ip = "192.168.56.101"
+username = "prne"
+password = "cisco123!"
+telnet_command = f"telnet {device_ip}"
+telnet_session = pexpect.spawn(telnet_command)
+telnet_session.expect("Username:")
+telnet_session.sendline(username)
+telnet_session.expect("Password:")
+telnet_session.sendline(password)
+telnet_session.expect("#")
+telnet_session.sendline("configure terminal")
+telnet_session.expect("#")
+
 acl_name = "my_acl"
 acl_rules = ["permit tcp any any eq 80", "deny ip any any"]
 
