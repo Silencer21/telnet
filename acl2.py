86e1bb17bd4603d7db1bcc18327ec7682352cdfa
Silencer21 <145554273+Silencer21@users.noreply.github.com>
2023-12-07 23:59:13 +0000

    Update acl2.py

--- acl2.py
+++ acl2.py
@@ -5,6 +5,9 @@ ip_address = '192.168.56.101'
 username = 'cisco'
 password = 'cisco123!'
 hostname = 'R1'
+crypto_map_name = 'MY_CRYPTO_MAP'
+transform_set_name = 'MY_TRANSFORM_SET'
+isakmp_key = 'cisco123!'
 
 # Create telnet session
 session = pexpect.spawn('telnet ' + ip_address, encoding='utf-8', timeout=20)
@@ -55,72 +58,42 @@ result = session.expect(([r'\(config\)#', pexpect.TIMEOUT, pexpect.EOF]))
 if result != 0:
     print('-' * 3, 'FAILURE setting hostname')
     exit()
-
 result = session.expect(([r'\(config\)#', pexpect.TIMEOUT, pexpect.EOF]))
 print('Hostname changed to: ', hostname)
 
 # Configure IPSec
-session.sendline('crypto isakmp policy 1')
-result = session.expect(([r'\(config-isakmp\)#', pexpect.TIMEOUT, pexpect.EOF]))
+session.sendline('crypto isakmp policy 10')
+session.sendline('authentication pre-share')
+session.sendline(f'encryption aes')
+session.sendline(f'hash sha')
+session.sendline(f'group 2')
+session.sendline(f'lifetime 3600')
+session.expect('#')
+
+session.sendline(f'crypto isakmp key {isakmp_key} address {ip_address}')
+
+session.sendline(f'crypto ipsec transform-set {transform_set_name} esp-aes esp-sha-hmac')
+session.sendline(f'crypto map {crypto_map_name} 10 ipsec-isakmp')
+session.sendline(f'set peer {ip_address}')
+session.sendline(f'set transform-set {transform_set_name}')
+session.sendline(f'match address 101')
+
+# Apply Crypto Map to Interface
+session.sendline(f'interface GigabitEthernet0/0')
+session.sendline(f'crypto map {crypto_map_name}')
+
+# Show running config to confirm IPSec configuration
+session.sendline('show running-config')
+session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
+
 if result != 0:
-    print('-' * 3, 'FAILURE configuring ISAKMP policy')
+    print('-' * 3, 'FAILURE showing running configuration')
     exit()
 
-session.sendline('encryption aes 256')
-result = session.expect(([r'\(config-isakmp\)#', pexpect.TIMEOUT, pexpect.EOF]))
-if result != 0:
-    print('-' * 3, 'FAILURE setting encryption for ISAKMP policy')
-    exit()
-
-session.sendline('hash sha256')
-result = session.expect(([r'\(config-isakmp\)#', pexpect.TIMEOUT, pexpect.EOF]))
-if result != 0:
-    print('-' * 3, 'FAILURE setting hash for ISAKMP policy')
-    exit()
-
-session.sendline('exit')
-result = session.expect(([r'\(config\)#', pexpect.TIMEOUT, pexpect.EOF]))
-if result != 0:
-    print('-' * 3, 'FAILURE exiting from IPSec configuration mode')
-    exit()
-
-session.sendline('crypto ipsec transform-set myset esp-aes 256 esp-sha256-hmac')
-result = session.expect(([r'\(config\)#', pexpect.TIMEOUT, pexpect.EOF]))
-if result != 0:
-    print('-' * 3, 'FAILURE configuring IPsec transform-set')
-    exit()
-
-session.sendline('exit')
-result = session.expect(([r'#', pexpect.TIMEOUT, pexpect.EOF]))
-if result != 0:
-    print('-' * 3, 'FAILURE exiting from config mode')
-    exit()
-
-# Show IPsec configuration
-session.sendline('show crypto isakmp policy')
-result = session.expect([r'#', pexpect.TIMEOUT, pexpect.EOF])
-if result != 0:
-    print('-' * 3, 'FAILURE showing ISAKMP policy')
-    exit()
-
-session.sendline('show crypto isakmp sa')
-result = session.expect([r'#', pexpect.TIMEOUT, pexpect.EOF])
-if result != 0:
-    print('-' * 3, 'FAILURE showing ISAKMP SA')
-    exit()
-
-session.sendline('show crypto ipsec transform-set')
-result = session.expect([r'#', pexpect.TIMEOUT, pexpect.EOF])
-if result != 0:
-    print('-' * 3, 'FAILURE showing IPsec transform-set')
-    exit()
-
-# Save running configuration to file
-session.sendline('write memory')
-result = session.expect(['[OK]', pexpect.TIMEOUT, pexpect.EOF])
-if result != 0:
-    print('-' * 3, 'FAILURE saving configuration')
-    exit()
+# Saving to local file
+with open('config.txt', 'w') as file:
+    file.write(session.before)
+print('Running configuration saved to config.txt')
 
 # Terminate telnet session
 session.sendline('quit')
