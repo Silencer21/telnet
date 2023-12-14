67d7d2ea0779765b82bd8786f575cc29c1399661
Silencer21 <145554273+Silencer21@users.noreply.github.com>
2023-12-14 02:29:10 +0000

    Update ssh-fix.py

--- ssh-fix.py
+++ ssh-fix.py
@@ -1,74 +1,115 @@
+
 import pexpect
+import difflib
+
 
 # Define variables
 ip_address = '192.168.56.101'
 username = 'prne'
 password = 'cisco123!'
 password_enable = 'class123!'
-new_hostname = 'R1'
+save_running = 'running_config.txt'
+save_startup = 'startup_config.txt'
 
-try:
-    # Create an SSH connection
-    session = pexpect.spawn('ssh -o "StrictHostKeyChecking 0" ' + username + '@' + ip_address, encoding='utf-8', timeout=20)
-    result = session.expect(['Password:', pexpect.TIMEOUT, pexpect.EOF])
+# Create the SSH session
+session = pexpect.spawn('ssh ' + username + '@' + ip_address,
+encoding='utf-8', timeout=20)
+result = session.expect(['Password:', pexpect.TIMEOUT, pexpect.EOF])
 
-    # Check for errors and exit if needed
-    if result != 0:
-        print('-' * 3, 'FAILURE! Creating a session for:', ip_address)
-        exit()
 
-    # Provide the password
-    session.sendline(password)
-    result = session.expect(['>', pexpect.TIMEOUT, pexpect.EOF])
+# Check for error, if exists then display error and exit
+if result != 0:
+    print('--- FAILURE! creating session for: ', ip_address)
+    exit()
 
-    # Check for errors and exit if needed
-    if result != 0:
-        print('-' * 3, 'FAILURE! Entering password:', password)
-        exit()
+# Session expecting password, enter details
+session.sendline(password)
+result = session.expect(['>', pexpect.TIMEOUT, pexpect.EOF])
+# Check for error, if exists then display error and exit
+if result != 0:
+    print('--- FAILURE! entering password: ', password)
+    exit()
 
-    # Enter enable mode
-    session.sendline('enable')
-    result = session.expect(['Password:', pexpect.TIMEOUT, pexpect.EOF])
 
-    # Check for errors and exit if needed
-    if result != 0:
-        print('-' * 3, 'FAILURE! Entering enable mode')
-        exit()
+# Enter enable mode
+session.sendline('enable')
+result = session.expect(['Password:', pexpect.TIMEOUT, pexpect.EOF])
+# Check for error, if exists then display error and exit
+if result != 0:
+    print('--- Failure! entering enable mode')
+    exit()
+# Send enable password details
+session.sendline(password_enable)
+result = session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
+# Check for error, if exists then display error and exit
+if result != 0:
+    print('--- Failure! entering enable mode after sending password')
+    exit()
 
-    # Send the enable password
-    session.sendline(password_enable)
-    result = session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
 
-    # Check for errors and exit if needed
-    if result != 0:
-        print('-' * 3, 'FAILURE! Entering enable mode after sending the password')
-        exit()
+# Enter configuration mode
+session.sendline('configure terminal')
+result = session.expect([r'.\(config\)#', pexpect.TIMEOUT, pexpect.EOF])
+# Check for error, if exists then display error and exit
+if result != 0:
+    print('--- Failure! entering config mode')
+    exit()
 
-    # Enter config mode
-    session.sendline('configure terminal')
-    result = session.expect([r'.\(config\)#', pexpect.TIMEOUT, pexpect.EOF])
 
-    # Check for errors and exit if needed
-    if result != 0:
-        print('-' * 3, 'FAILURE! Entering config mode')
-        exit()
+# Change the hostname to R1
+session.sendline('hostname R1')
+result = session.expect([r'R1\(config\)#', pexpect.TIMEOUT, pexpect.EOF])
+# Check for error, if exists then display error and exit
+if result != 0:
+    print('--- Failure! setting hostname')
 
-    # Change the hostname
-    session.sendline('hostname ' + new_hostname)
-    result = session.expect([r'{}\(config\)#'.format(new_hostname), pexpect.TIMEOUT, pexpect.EOF])
+# Show running-config
+session.sendline('exit')
+session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
+session.sendline('terminal length 0')
+session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
+session.sendline('show running-config')
+session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
 
-    # Check for errors and exit if needed
-    if result != 0:
-        print('-' * 3, 'FAILURE! Setting hostname')
-        exit()
+# saving the file locally
+with open(save_running, 'w') as file:
+    file.write(session.before)
 
-    # Save the configuration to the local device
-    session.sendline('write memory')
-    result = session.expect([r'.\(config\)#', pexpect.TIMEOUT, pexpect.EOF])
+#Show startup-config
+session.sendline('show startup-config')
+session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
 
-    # Check for errors and exit if needed
-    if result != 0:
-        print('-' * 3, 'FAILURE! Saving the configuration')
+# Saving the startup file locally
+with open(save_startup, 'w') as file :
+    file.write(session.before)
 
-except Exception as e:
-    print('-' * 3, 'SSH connection failed:', str(e))
+running_file = open(save_running, 'r')
+running_data = running_file.readlines()
+
+startup_file = open(save_startup, 'r')
+startup_data = startup_file.readlines()
+
+
+diff = difflib.ndiff(running_data, startup_data)
+#diffs_file = 'diffs_file.txt'
+for diffs in diff:
+    print(diffs)
+ # with open (diffs_file, 'a') as file:
+  #
+  #       file.write(diffs)
+
+# Exit config mode
+session.sendline('exit')
+# Exit enable mode
+session.sendline('exit')
+
+print('-------------------------------------------------------------------------')
+print('')
+print('--- You have logged in succesfully! connecting to:', ip_address)
+print('---                                      Username: ' , username)
+print('---                                      Password: ' , password)
+print('')
+print('-------------------------------------------------------------------------')
+
+# Terminate SSH session
+session.close()
